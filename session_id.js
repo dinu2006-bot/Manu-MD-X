5e91424ba66183d4abb4c5e6def9e5ab84220cc3
Dinu Production <theekshanedinuware@gmail.com>
2024-12-24 11:13:48 +0530

    Update session_id.js

--- session_id.js
+++ session_id.js
@@ -43,5 +43,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id",
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&CUsyhKKL#ndEW2GV9SRlXlJ9sLcvExOdOrTn6q3xbFDEaL5pKzmE",
 };
